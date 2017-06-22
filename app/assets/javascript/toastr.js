381e2a546fa0b976771a7c666abd9b8ebab68ef8
Igor Rezende <igorgesso@gmail.com>
2017-06-22 16:30:26 -0300

    ajustes posição

--- app/assets/javascript/toastr.js
+++ app/assets/javascript/toastr.js
@@ -178,8 +178,8 @@
                         warning: 'toast-warning'
                     },
                     iconClass: 'toast-info',
-                    positionClass: 'toast-top-right',
-                    timeOut: 5000, // Set timeOut and extendedTimeOut to 0 to make it sticky
+                    positionClass: 'toast-top-full-width',
+                    timeOut: 15000, // Set timeOut and extendedTimeOut to 0 to make it sticky
                     titleClass: 'toast-title',
                     messageClass: 'toast-message',
                     escapeHtml: false,
